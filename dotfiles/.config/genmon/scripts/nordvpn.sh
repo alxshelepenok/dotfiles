97d5f94153eaeb31679beacf9c59070bac63292b
Alexander Shelepenok <alxshelepenok@gmail.com>
2023-02-10 23:42:53 +0000

    chore(backup): sync changes

--- dotfiles/.config/genmon/scripts/nordvpn.sh
+++ dotfiles/.config/genmon/scripts/nordvpn.sh
@@ -4,7 +4,7 @@ NORDVPN_STATUS="$(nordvpn status)"
 CONNECTED=$(echo "$NORDVPN_STATUS" 2>/dev/null | grep -o -P '(?<=Status: ).*')
  
 if [ "$CONNECTED" == "Connected" ]; then
-  CURRENT_SERVER=$(echo "$NORDVPN_STATUS" | grep -o -P '(?<=Current server: ).*')
+  CURRENT_SERVER=$(echo "$NORDVPN_STATUS" | grep -o -P '(?<=Hostname: ).*')
   CURRENT_CITY=$(echo "$NORDVPN_STATUS" | grep -o -P '(?<=City: ).*')
   INFO="<icon>network-vpn-symbolic</icon>"
   INFO+="<txt> ${CURRENT_CITY} / ${CURRENT_SERVER}</txt>"
--- dotfiles/scripts/nordvpn/nordvpn.sh
+++ dotfiles/scripts/nordvpn/nordvpn.sh
@@ -2,14 +2,12 @@
 
 if [ "$1" == "-e" ];
 then
-  nordvpn set killswitch disabled
   nordvpn connect germany
-  nordvpn set killswitch enabled
-  nordvpn settings
 elif [ "$1" == "-d" ];
 then
-  nordvpn set killswitch disabled
   nordvpn disconnect
-  nordvpn set killswitch enabled
-  nordvpn settings
 fi
+
+nordvpn set autoconnect enabled germany
+nordvpn set killswitch enabled
+nordvpn settings
